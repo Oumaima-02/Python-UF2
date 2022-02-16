e15a5ed487225d23c1b4d88545426933a6b62ea0
Oumaima-02 <95426206+Oumaima-02@users.noreply.github.com>
2022-02-16 16:51:33 +0100

    Delete pythonProject[Exercici6] directory

--- pythonProject[Exercici6]/functions.py
+++
@@ -1,9 +0,0 @@
-def num_seq(num):
-    i, sum = 0, 0
-    llista = list()
-    while i < num:
-        sum = sum + i
-        if sum < num:
-            llista.append(i)
-        i = i + 1
-    return llista
